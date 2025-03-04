6e54f49caa4498f26ca219712b014fe184dda224
Ratnesh8577 <ratneshchauhan279@gmail.com>
2025-03-04 23:01:37 +0530

    Add files via upload

---
+++ LINKLIST(INSERTATBEGINNING).py
@@ -0,0 +1,31 @@
+
+#INsertion of node in front (Linked list)
+class Node:
+    def __init__ (self,data):
+        self.data=data
+        self.next=None
+class Linkedlist:
+    def __init__(self):
+        self.head=None
+#insert at the front of the link list
+    def insertatbegginning(self,new_data):
+        #Create of the new node
+        new_node=Node(new_data)
+        #insertion at the begginning
+        new_node.next=self.head
+        self.head=new_node
+    #print the linkedlist
+    def printlist(self):
+        temp=self.head
+        while temp:
+            print(str(temp.data) +" ",end=" ")
+            temp=temp.next
+
+#Driver code
+llist=Linkedlist()
+llist.insertatbegginning(12)
+llist.insertatbegginning(1)
+llist.insertatbegginning(7)
+llist.insertatbegginning(9)
+llist.insertatbegginning(10)
+llist.printlist()
