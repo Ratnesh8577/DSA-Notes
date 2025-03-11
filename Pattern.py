36b3e9725cd5f76a2fb1b24fdaec784bc88019d6
Ratnesh8577 <ratneshchauhan279@gmail.com>
2025-03-11 23:28:05 +0530

    Add files via upload

---
+++ Pattern.py
@@ -0,0 +1,35 @@
+"""(1)
+n=int(input("Enter the row number: "))
+for i in range(n):
+    for j in range(i+1):
+        print("*",end=" ")
+    print()
+output
+Enter the row number: 5
+* 
+* *
+* * *
+* * * *
+* * * * *"""
+
+#(2)
+"""n=int(input("Enter the row number: "))
+for i in range(n,0,-1):
+    for j in range(i,0,-1):
+        print("*",end=" ")
+    print()
+
+output
+Enter the row number: 5
+* * * * * 
+* * * *
+* * *
+* *
+*
+"""
+
+
+
+
+
+
