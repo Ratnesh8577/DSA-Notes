2b5d0b835ef74456d188b3f1fbd2c20f564076e6
Ratnesh8577 <ratneshchauhan279@gmail.com>
2025-04-10 20:03:17 +0530

    Add files via upload

---
+++ 1)hashmap.py
@@ -0,0 +1,17 @@
+# Hashmap(Dictionary) :- Hashmap is a collection of key and value pair
+
+# key never be duplicate but value same 2 key or more key
+
+dict = {1:"India",2:"England",3:"Aus"}
+# dict[key] = value (update the value)
+dict[4] = "Pakistan"
+
+for v in dict.values():
+    print(v)
+
+for k in dict.keys():
+    print(k)
+
+for k,v in dict.items():
+    print(k,v)
+
